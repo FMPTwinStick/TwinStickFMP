03365811d4f1bd4e350f936b6ac2afffa0e4bc23
NBDZero <51916630+NBDZero@users.noreply.github.com>
2020-04-07 15:33:34 +0100

    Incorporated dual controls into tankcontroller

---
+++ 3D Twin Stick Proto/Assets/Scripts/TankTwoController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankTwoController : MonoBehaviour
+{
+    //Public Game Objects:
+    public GameObject bulletObject;
+    public Component cannonPivot;
+
+    //Private Class Variables:
+    private Vector3 moveInput;
+    private float moveSpeed;
+
+    private float timeBetweenShots;
+    private float timePassed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 10f;
+
+        timeBetweenShots = 0.15f;
+        timePassed = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        RightStick();
+    }
+    //Movement Function:
+    void Movement()
+    {
+        //Getting the input from the controller:
+        moveInput = new Vector3(Input.GetAxisRaw("HorizontalJ2"), 0f, Input.GetAxisRaw("VerticalJ2"));
+
+        transform.position = transform.position + moveInput * moveSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.LookRotation(moveInput, Vector3.up);
+    }
+
+    //Rotating Function:
+    void RightStick()
+    {
+        //Getting input from controller:
+        Vector3 playerDirection = new Vector3(Input.GetAxisRaw("RHorizontalJ2"), 0f, -Input.GetAxisRaw("RVerticalJ2"));
+
+        //Checking for any input at all:
+        if (playerDirection.sqrMagnitude > 0f)
+        {
+            //rotating player:
+            cannonPivot.transform.rotation = Quaternion.LookRotation(playerDirection, Vector3.up);
+
+            //Shooting:
+            if (timePassed > timeBetweenShots)
+            {
+                timePassed = 0;
+
+                Instantiate(bulletObject, transform.position + playerDirection, Quaternion.LookRotation(playerDirection, Vector3.up));
+                
+            }
+        }
+
+        timePassed += Time.deltaTime;
+    }
+
+}
