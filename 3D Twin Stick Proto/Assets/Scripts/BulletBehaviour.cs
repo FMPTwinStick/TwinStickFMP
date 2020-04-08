7630d2ea587cdc8df8dcd9676d9b698ddadce4b2
NBDZero <51916630+NBDZero@users.noreply.github.com>
2020-04-08 16:06:20 +0100

    Added bouncing bullets

--- 3D Twin Stick Proto/Assets/Scripts/BulletBehaviour.cs
+++ 3D Twin Stick Proto/Assets/Scripts/BulletBehaviour.cs
@@ -9,6 +9,7 @@ public class BulletBehaviour : MonoBehaviour
 
     private float   moveSpeed;
     private int     maxBounces;
+    private int moveTimes  = 3;
     private int     currentBounces;
 
     // Start is called before the first frame update
@@ -16,51 +17,61 @@ public class BulletBehaviour : MonoBehaviour
     {
         //Initialising Variables:
         moveSpeed = 20;
-
+        m_rigidbody = GetComponent<Rigidbody>();
         maxBounces = 100;
         currentBounces = 0;
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement();
-    }
+       Movement();
+       
+     }
 
     //Collisions with walls:
     void OnCollisionEnter( Collision collision )
     {
-        if (collision.gameObject.tag == "VerticalWall")
-        {
-            if (maxBounces >= currentBounces)
-            {
-                currentBounces++;
-                transform.rotation = Quaternion.Euler(transform.rotation.x,(360f-transform.rotation.y*(180f/Mathf.PI)),transform.rotation.z);
-            }
+        //if (collision.gameObject.tag == "VerticalWall")
+        //{
+        //    if (maxBounces >= currentBounces)
+        //    {
+        //        currentBounces++;
+        //        transform.rotation = Quaternion.Euler(transform.rotation.x,(360f-transform.rotation.y*(180f/Mathf.PI)),transform.rotation.z);
+        //    }
 
-            if (maxBounces <= currentBounces)
-            {
-                Destroy(gameObject);
-            }
-        }
+        //    if (maxBounces <= currentBounces)
+        //    {
+        //        Destroy(gameObject);
+        //    }
+        //}
 
-        if (collision.gameObject.tag == "HorizontalWall")
-        {
-            if (maxBounces >= currentBounces)
-            {
-                Destroy(gameObject);
-            }
+        //if (collision.gameObject.tag == "HorizontalWall")
+        //{
+        //    if (maxBounces >= currentBounces)
+        //    {
+        //        Destroy(gameObject);
+        //    }
 
-            if (maxBounces <= currentBounces)
-            {
-                Destroy(gameObject);
-            }
-        }
+        //    if (maxBounces <= currentBounces)
+        //    {
+        //        Destroy(gameObject);
+        //    }
+        //}
     }
 
     //Bullet Movement:
     void Movement()
     {
-        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        // transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        while (moveTimes >=0 )
+        {
+            m_rigidbody.AddForce(transform.forward * 10.0f);
+            moveTimes -= 1;
+        }
+          
+      
     }
 }
