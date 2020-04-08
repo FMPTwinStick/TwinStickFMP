deb3448633c2c28036cb869de7cf68dac0f1cea9
LewisNicoll1 <52451661+LewisNicoll1@users.noreply.github.com>
2020-04-08 14:34:54 +0100

    BulletBouncingWork (incomplete)

--- 3D Twin Stick Proto/Assets/Scripts/BulletBehaviour.cs
+++ 3D Twin Stick Proto/Assets/Scripts/BulletBehaviour.cs
@@ -7,15 +7,18 @@ public class BulletBehaviour : MonoBehaviour
     //Private Variables:
     private Rigidbody m_rigidbody;
 
-    private float moveSpeed;
+    private float   moveSpeed;
+    private int     maxBounces;
+    private int     currentBounces;
 
     // Start is called before the first frame update
     void Start()
     {
         //Initialising Variables:
-        m_rigidbody = GetComponent<Rigidbody>();
-
         moveSpeed = 20;
+
+        maxBounces = 100;
+        currentBounces = 0;
     }
 
     // Update is called once per frame
@@ -27,9 +30,31 @@ public class BulletBehaviour : MonoBehaviour
     //Collisions with walls:
     void OnCollisionEnter( Collision collision )
     {
-        if (collision.gameObject.tag == "Wall")
+        if (collision.gameObject.tag == "VerticalWall")
         {
-            Destroy(gameObject);
+            if (maxBounces >= currentBounces)
+            {
+                currentBounces++;
+                transform.rotation = Quaternion.Euler(transform.rotation.x,(360f-transform.rotation.y*(180f/Mathf.PI)),transform.rotation.z);
+            }
+
+            if (maxBounces <= currentBounces)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        if (collision.gameObject.tag == "HorizontalWall")
+        {
+            if (maxBounces >= currentBounces)
+            {
+                Destroy(gameObject);
+            }
+
+            if (maxBounces <= currentBounces)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 
