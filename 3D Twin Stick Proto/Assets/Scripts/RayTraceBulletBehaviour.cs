fa977b562a17d5be1e4a7b6bfac56f402266f4bd
LewisNicoll1 <52451661+LewisNicoll1@users.noreply.github.com>
2020-04-12 17:02:51 +0100

    Raycast Bullet Script Changes
    Raycast Bullet Script Changes

--- 3D Twin Stick Proto/Assets/Scripts/RayTraceBulletBehaviour.cs
+++ 3D Twin Stick Proto/Assets/Scripts/RayTraceBulletBehaviour.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class RayTraceBulletBehaviour : MonoBehaviour
 {
+    //Public Variables:
+    public int maxBounces;
+
     //Private Variables:
-    private float moveSpeed;
+    private float   moveSpeed;
+
+    private Ray bulletPath;
+    private RaycastHit objectHit;
+    private Vector3 reflectDirection;
+    private float newRotation;
+
+    private int currentBounces;
 
     // Start is called before the first frame update
     void Start()
     {
-        moveSpeed = 5;
+        moveSpeed       = 5;
+
+        bulletPath      = new Ray(transform.position, transform.forward);
+
+        currentBounces  = 0;
     }
 
     // Update is called once per frame
@@ -28,13 +42,13 @@ public class RayTraceBulletBehaviour : MonoBehaviour
 
     void Reflections()
     {
-        Ray bulletPath = new Ray(transform.position, transform.forward);
-        RaycastHit objectHit;
-
-        if( Physics.Raycast(bulletPath,out objectHit, moveSpeed * Time.deltaTime +0.5f))
+        bulletPath.origin = transform.position;
+        bulletPath.direction = transform.forward;
+        
+        if( Physics.Raycast(bulletPath,out objectHit, moveSpeed * Time.deltaTime + .1f))
         {
-            Vector3 reflectDirection = Vector3.Reflect(bulletPath.direction, objectHit.normal);
-            float newRotation = 90f - Mathf.Atan2(reflectDirection.z, reflectDirection.x) * Mathf.Rad2Deg;
+            reflectDirection = Vector3.Reflect(bulletPath.direction, objectHit.normal);
+            newRotation = 90f - Mathf.Atan2(reflectDirection.z, reflectDirection.x) * Mathf.Rad2Deg;
             transform.position = objectHit.point;
             transform.eulerAngles = new Vector3(0, newRotation, 0);
         }
