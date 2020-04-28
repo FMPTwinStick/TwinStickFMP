b5de54bac3ce90053c9f98150df0c3d7dc6f8559
azali2012 <azali7865@gmail.com>
2020-04-28 16:31:46 +0100

    Added Menu
    UI

---
+++ 3D Twin Stick Proto/Assets/Scripts/UI Sripts/DeathScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScreen : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log("Quit!!!");
+        Application.Quit();
+    }
+
+    public void LoadMainMenu()
+    {
+        Debug.Log("Loading Menu");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2 );
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
