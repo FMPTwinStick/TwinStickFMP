f438f52ce347601de23a3297e39cab80f1eb61e1
azali2012 <azali7865@gmail.com>
2020-07-13 17:19:14 +0100

    Co-op menu

--- 3D Twin Stick Proto with Menu/Assets/Scripts/UI Sripts/ClassicLevels.cs
+++ 3D Twin Stick Proto with Menu/Assets/Scripts/UI Sripts/ClassicLevels.cs
@@ -78,4 +78,52 @@ public class ClassicLevels : MonoBehaviour
     {
         SceneManager.LoadScene("ClassicLevel12");
     }
+
+    //This loads the Classic Level 13
+    public void LoadClassicLevel13()
+    {
+        SceneManager.LoadScene("ClassicLevel13");
+    }
+
+    //This loads the Classic Level 14
+    public void LoadClassicLevel14()
+    {
+        SceneManager.LoadScene("ClassicLevel14");
+    }
+
+    //This loads the Classic Level 15
+    public void LoadClassicLevel15()
+    {
+        SceneManager.LoadScene("ClassicLevel15");
+    }
+
+    //This loads the Classic Level 16
+    public void LoadClassicLevel16()
+    {
+        SceneManager.LoadScene("ClassicLevel16");
+    }
+
+    //This loads the Classic Level 17
+    public void LoadClassicLevel17()
+    {
+        SceneManager.LoadScene("ClassicLevel17");
+    }
+
+    //This loads the Classic Level 18
+    public void LoadClassicLevel18()
+    {
+        SceneManager.LoadScene("ClassicLevel18");
+    }
+
+    //This loads the Classic Level 19
+    public void LoadClassicLevel19()
+    {
+        SceneManager.LoadScene("ClassicLevel19");
+    }
+
+    //This loads the Co-op Classic Level 20
+    public void LoadClassicLeve20()
+    {
+        SceneManager.LoadScene("ClassicLevel20");
+    }
 }
