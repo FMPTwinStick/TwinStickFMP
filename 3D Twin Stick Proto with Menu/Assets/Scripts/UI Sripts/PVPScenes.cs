3bd9035781e2d0ee2d6066475ac8dfbd27d8012e
azali2012 <azali7865@gmail.com>
2020-07-20 17:21:22 +0100

    RandomSceneLoader

---
+++ 3D Twin Stick Proto with Menu/Assets/Scripts/UI Sripts/PVPScenes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PVPScenes : MonoBehaviour
+{
+    public void LoadRandomScene()
+    {
+        int Index = Random.Range(44, 53);
+        SceneManager.LoadScene(Index);
+        Debug.Log("RandomSceneLoaded");
+    }
+}
